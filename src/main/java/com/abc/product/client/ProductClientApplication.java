4ca718d7381f0e3db5db743efc7a87fb24ac4821
Amlan Deep Nandi <amlandeep.nandi@gmail.com>
2025-05-15 19:50:04 +0530

    Adding timeouts in RestTemplate
    Adding timeouts in RestTemplate definition

--- src/main/java/com/abc/product/client/ProductClientApplication.java
+++ src/main/java/com/abc/product/client/ProductClientApplication.java
@@ -1,7 +1,10 @@
 package com.abc.product.client;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -21,8 +24,11 @@ public class ProductClientApplication
 
 	@LoadBalanced
 	@Bean
-	RestTemplate restTemplate()
+	RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder)
 	{
-		return new RestTemplate();
+		return restTemplateBuilder
+				.connectTimeout(Duration.ofSeconds(500))
+				.readTimeout(Duration.ofSeconds(500))
+				.build();
 	}
 }
